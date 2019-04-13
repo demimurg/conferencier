98c81e1e13e228f0779ddaa9399b2c7f527d7588
Alex <blohinalexej@mail.ru>
2019-04-14 01:46:43 +0300

    n + 1 try to deploy on heroku

--- main.js
+++ main.js
@@ -161,28 +161,24 @@ bot.on('location', async (ctx) => {
 
 
 process.on('launch', async () => {
-	try {
-		await db.init()
-		console.log('База данных подключена')
+	await db.init()
+	console.log('База данных подключена')
 
-		let mode
-		if (process.env.PLATFORM === 'heroku') {
-			mode = {
-				webhook: {
-					domain: process.env.URL,
-					hookPath: `/bot${process.env.TOKEN}`,
-					port: process.env.PORT
-				}
+	let mode
+	if (process.env.PLATFORM === 'heroku') {
+		mode = {
+			webhook: {
+				domain: process.env.URL,
+				hookPath: `/bot${process.env.TOKEN}`,
+				port: process.env.PORT
 			}
-		} else {
-			mode = { polling: {} }
 		}
-
-		bot.launch(mode)
-		bot.catch((err) => console.log(err))
-	} catch (err) {
-		console.log(err)
+	} else {
+		mode = { polling: {} }
 	}
+
+	bot.launch(mode)
+	bot.catch((err) => console.log(err))
 })
 process.on('SIGINT', async () => {
 	console.log('\nCоединение с базой данных разорвано')
